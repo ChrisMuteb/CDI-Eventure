3ef7837da2bfe88c49928829c102c4a8844c557e
chriskayomb <ibond.chris@gmail.com>
2024-06-16 14:53:54 +0200

    return a X-XSRF-TOKEN to the user and set the cors

--- src/main/java/com/lasuperbe/server/security/EventureSecurityConfiguration.java
+++ src/main/java/com/lasuperbe/server/security/EventureSecurityConfiguration.java
@@ -1,11 +1,13 @@
 package com.lasuperbe.server.security;
 
+import com.lasuperbe.server.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +15,9 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -24,8 +29,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class EventureSecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        // 1) All requests should be authenticated
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        http.securityContext((context) -> context
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
             @Override
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration config = new CorsConfiguration();
@@ -36,16 +46,14 @@ public class EventureSecurityConfiguration {
                 config.setMaxAge(3600L);
                 return config;
             }
-        }));
-        http.authorizeHttpRequests(
-                auth -> auth.requestMatchers("v1/events","v1/users", "v1/tasks", "v1/participants").authenticated()
+        })).csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers( "/contact", "v1/register")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("v1/events","v1/users", "v1/tasks", "v1/participants").authenticated()
                         .requestMatchers("v1/register").permitAll()
-        );
-        // 2) If a request is not authenticated, a web page is shown
-        http.httpBasic(withDefaults());
-        // 3) CSRF -> POST, PUT
-        http.formLogin(Customizer.withDefaults());
-        http.csrf(AbstractHttpConfigurer::disable);
+        ).formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
